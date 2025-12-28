b37ed86076c073167fb6372798c8a51e7b599bfb
unbalancedparen <unbalancedparen@unbalancedparens-iMac.local>
2025-12-28 20:05:18 -0300

    Add GitHub Actions deployment

---
+++ flake.nix
@@ -0,0 +1,34 @@
+{
+  description = "Federico Carrone's personal website";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            zola
+            nodejs_20
+          ];
+
+          shellHook = ''
+            echo "federicocarrone.com dev environment"
+            echo ""
+            echo "Commands:"
+            echo "  zola serve        - Start dev server at http://127.0.0.1:1111"
+            echo "  zola build        - Build static site"
+            echo "  npm install       - Install Node dependencies"
+            echo "  npm run build-css - Minify CSS"
+            echo ""
+          '';
+        };
+      }
+    );
+}
